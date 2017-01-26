416ee9ebf8b595ffcffb098a8737792b4408528e
Marius Wodtke <marius.wodtke@gmail.com>
2017-01-26 17:07:32 +0100

    added script for listening to probe requests

---
+++ shark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ifconfig wlp1s0 down
+iwconfig wlp1s0 mode monitor
+ifconfig wlp1s0 up
+
+tshark -S -l -i wlp1s0 -Y 'wlan.fc.type_subtype eq 4' -T fields -e frame.time_epoch -e wlan.sa_resolved -e wlan.sa -e radiotap.dbm_antsignal
