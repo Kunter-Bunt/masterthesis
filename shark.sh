d4d2e3cc5a05b8701366d6738c19b5e3ea5d54f2
Marius Wodtke <marius.wodtke@gmail.com>
2017-02-15 17:20:29 +0100

    modified probereq sketch to get desired functionality & added new fields for bssid on the tshark script

--- shark.sh
+++ shark.sh
@@ -4,4 +4,4 @@ ifconfig wlp1s0 down
 iwconfig wlp1s0 mode monitor
 ifconfig wlp1s0 up
 
-tshark -S -l -i wlp1s0 -Y 'wlan.fc.type_subtype eq 4' -T fields -e frame.time_epoch -e wlan.sa_resolved -e wlan.sa -e radiotap.dbm_antsignal
+tshark -S -l -i wlp1s0 -Y 'wlan.fc.type_subtype eq 4' -T fields -e frame.time_epoch -e wlan.sa_resolved -e wlan.sa -e radiotap.dbm_antsignal  -e wlan.bssid_resolved -e wlan.bssid
