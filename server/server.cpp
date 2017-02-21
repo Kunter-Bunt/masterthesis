8b07901753aafd0ef5db3a88dd15adc8f84b1448
Marius Wodtke <marius.wodtke@gmail.com>
2017-02-21 07:44:45 +0100

    Added support for reading files from the arguments

--- server/server.cpp
+++ server/server.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
 using namespace std;
 
-int main() {
+bool working = true;
+
+int main(int argc, char** argv) {
 	cout << "Starting..." << endl;
+	
+	string inputs[argc];
+	for (int i = 0; i < argc; i++) {
+		cout << "Monitoring " << argv[i] << endl;
+		inputs[i] = argv[i];
+	}
+
+
+	string dataPoint;
+	ifstream tsharkInput(inputs[1].c_str());
+	if (tsharkInput.is_open()){
+		while (working){
+			if (getline(tsharkInput, dataPoint)) {
+				cout << dataPoint << '\n';
+			}
+			else {
+				sleep(1);
+				tsharkInput.clear();
+			}
+		}
+		tsharkInput.close();
+	}
+	else cout << "Cannot open file" << endl;
 	return 0;
 }
 
