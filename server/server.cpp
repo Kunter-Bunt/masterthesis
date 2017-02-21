6e660d8b851a3741f3a6b4a9eafdaeaab6a19a55
Marius Wodtke <marius.wodtke@gmail.com>
2017-02-21 12:59:41 +0100

    server now takes multiple file arguments and prints them like cat, but also works for streams, pipe output of commands to it with <(command), one thread per file, added thread support in makefile

--- server/server.cpp
+++ server/server.cpp
@@ -2,22 +2,15 @@
 #include <fstream>
 #include <string>
 #include <unistd.h>
+#include <thread>
 using namespace std;
 
 bool working = true;
 
-int main(int argc, char** argv) {
-	cout << "Starting..." << endl;
-	
-	string inputs[argc];
-	for (int i = 0; i < argc; i++) {
-		cout << "Monitoring " << argv[i] << endl;
-		inputs[i] = argv[i];
-	}
-
-
+void monitor(string file) {
 	string dataPoint;
-	ifstream tsharkInput(inputs[1].c_str());
+	ifstream tsharkInput(file);
+
 	if (tsharkInput.is_open()){
 		while (working){
 			if (getline(tsharkInput, dataPoint)) {
@@ -31,6 +24,24 @@ int main(int argc, char** argv) {
 		tsharkInput.close();
 	}
 	else cout << "Cannot open file" << endl;
+}
+
+int main(int argc, char** argv) {
+	cout << "Starting..." << endl;
+	
+	//set up workers
+	string file[argc-1];
+	thread worker[argc-1];
+	for (int i = 0; i < argc-1; i++) {
+		cout << "Monitoring " << argv[i+1] << endl;
+		file[i] = string(argv[i+1]);
+		worker[i] = thread(monitor, file[i]);
+	}
+	
+	//Cleaning up
+	for (int i = 0; i < argc-1; i++) {
+		worker[i].join();
+	}
 	return 0;
 }
 
