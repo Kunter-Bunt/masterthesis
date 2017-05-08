a2775096bd29be597d2e7366fb113ece9e081d4b
Marius Wodtke <marius.wodtke@gmail.com>
2017-05-08 14:41:03 +0200

    VccRead now properly reads the battery, cant read bat and chip vcc at the same time!

--- sketches/VccRead/VccRead.ino
+++ sketches/VccRead/VccRead.ino
@@ -2,23 +2,30 @@ extern "C" {
 #include "user_interface.h"
 }
 
-ADC_MODE(ADC_VCC);
+//ADC_MODE(ADC_VCC);
 
 int vdd;
+int bat;
+int level;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {
-    ;
-  }
+  while (!Serial); 
   Serial.println();
-  vdd = ESP.getVcc();
-  Serial.println(vdd);
 }
 
 void loop() {
+  //what comes from battery
+  bat = analogRead(A0);
+  //level = map(level, 580, 774, 0, 100);
+  //Serial.print("Battery level: "); Serial.print(level); Serial.println("%");
+  Serial.println(bat);
+  //what gets to the chip, needs ADC_MODE(ADC_VCC); but that breaks the code above
+  /*
   vdd = ESP.getVcc();
   Serial.println(vdd);
-  delay(5000);
+  */
+  
+  delay(1000);
 }
 
