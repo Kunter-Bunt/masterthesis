fbba92f24e8268e214d6579103e6313544416ff0
Marius Wodtke <marius.wodtke@gmail.com>
2017-05-23 13:49:56 +0200

    Fixed some bugs and optimized WiFiLLSTag

--- esptool/WiFiLLSTag/WiFiLLSTag.c
+++ esptool/WiFiLLSTag/WiFiLLSTag.c
@@ -1,32 +1,34 @@
 #include "os_type.h" //os_timer_t
 #include "osapi.h" //os_timer_setfn, os_timer_arm
 #include "user_interface.h" //wifi_send_pkt_freedom
-#include "gpio.h"
-#include "espconn.h"
+#include "espconn.h" //udp stuff
 	
 struct espconn sendResponse;
-esp_udp udp;
+struct station_config stationConfig;
+//struct scan_config scanC;
 
+esp_udp udp;
 
 char buf[256];
 int len;
 
-static volatile os_timer_t connect_timer, send_timer;
+static volatile os_timer_t connect_timer, build_timer, send_timer;
 
 void scan_done(void *arg, STATUS status)
 {
     struct bss_info *bss_link = (struct bss_info *)arg;
-    bss_link = bss_link->next.stqe_next;//ignore the first one , it's invalid.
-
+    bss_link = bss_link->next.stqe_next;
+    
     while (bss_link != NULL)
     {
-      if (len < 220) len += os_sprintf(buf+l, " %02x:%02x:%02x:%02x:%02x:%02x %4d;", MAC2STR(bss_link->bssid), bss_link->rssi);
+      if (len < 220) len += os_sprintf(buf+len, " %02x:%02x:%02x:%02x:%02x:%02x %4d;", MAC2STR(bss_link->bssid), bss_link->rssi);
       bss_link = bss_link->next.stqe_next;  
     }
-    
+	
     espconn_send(&sendResponse, buf, len);
-    //wifi_set_sleep_type(LIGHT_SLEEP_T);
+	wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
+
 
 void connect(void *arg) {
 	wifi_station_connect();
@@ -42,18 +44,19 @@ void build(void *arg) {
 }
 
 void send_UDP(void *arg) {
- 	len = 0
-	len += += os_sprintf(buf, "5c:cf:7f:c6:a0:f1; ");
-	wifi_station_scan(NULL,scan_done);
-	//wifi_set_sleep_type(LIGHT_SLEEP_T);
+ 	len = 0;
+	len += os_sprintf(buf, "5c:cf:7f:c6:a0:f1; ");
+	
+	wifi_station_scan(NULL, scan_done);
+	wifi_set_sleep_type(LIGHT_SLEEP_T);
 }
 
 void initDone() {
-	wifi_set_opmode_current(STATION_MODE);
-	struct station_config stationConfig;
 	strncpy(stationConfig.ssid, "Lothlorien", 32);
 	strncpy(stationConfig.password, "JarJarBinks&R2D2", 64);
 	wifi_station_set_config(&stationConfig);
+	
+	//scanC.channel = (uint8)1;
 }
 
 void ICACHE_FLASH_ATTR user_init() {
