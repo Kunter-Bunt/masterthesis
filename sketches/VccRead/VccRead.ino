86aedd4ce7041ca00c1c0ba14569918945f39e4d
Marius Wodtke <marius.wodtke@gmail.com>
2017-07-05 14:18:42 +0200

    Changed the pins on VccRead

--- sketches/VccRead/VccRead.ino
+++ sketches/VccRead/VccRead.ino
@@ -1,5 +1,6 @@
 extern "C" {
 #include "user_interface.h"
+#include "gpio.h"
 }
 
 //ADC_MODE(ADC_VCC);
@@ -26,9 +27,17 @@ void loop() {
   vdd = ESP.getVcc();
   Serial.println(vdd);
   */
-
-  if(digitalRead(12) == HIGH) Serial.println("HIGH");
+  /*
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U , FUNC_GPIO15);
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
+  GPIO_DIS_OUTPUT(BIT15);
+  PIN_PULLUP_EN(PERIPHS_IO_MUX_MTDO_U); 
   
+  if (!GPIO_INPUT_GET(15)) Serial.println("LOW");
+  */
+  if(digitalRead(2) == LOW) digitalWrite(15, HIGH);//Serial.println("LOW");
+
   delay(1000);
+  digitalWrite(15, HIGH);
 }
 
