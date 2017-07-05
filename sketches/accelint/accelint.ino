e4d31b6ba880187e66e48caa7dedb65c40436794
Marius Wodtke <marius.wodtke@gmail.com>
2017-07-05 14:19:23 +0200

    Changed accelint for easy threshhold manipulation

--- sketches/accelint/accelint.ino
+++ sketches/accelint/accelint.ino
@@ -11,7 +11,7 @@
 #define LIS3DH_MOSI 11
 // Used for hardware & software SPI
 #define LIS3DH_CS 10
-int intPin = 16;
+int intPin = 12;
 int LIS3DH_ADDR = 0x18; // change this to 0x19 for alternative i2c address
 int reading = 0; //counter for number of readings taken - for diagnostic use
 
@@ -50,14 +50,20 @@ void writeRegister(byte reg, byte data) {
 void init_ACC(void)
 {
     // configurations for control registers
-    writeRegister(0x20, 0xA7); //Write A7h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode. 8F for low power mode
+    writeRegister(0x20, 0x2F); //Write A7h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode. 8F for low power mode
     writeRegister(0x21, 0x09); //Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
     writeRegister(0x22, 0x40); //Write 40h into CTRL_REG3;      // ACC AOI1 interrupt signal is routed to INT1 pin.
     writeRegister(0x23, 0x00); //Write 00h into CTRL_REG4;      // Full Scale = +/-2 g
     writeRegister(0x24, 0x08); //Write 08h into CTRL_REG5;      // Default value is 00 for no latching. Interrupt signals on INT1 pin is not latched. 
                                                                 //Users don’t need to read the INT1_SRC register to clear the interrupt signal.
     // configurations for wakeup and motionless detection
-    writeRegister(0x32, 0x14); //Write 10h into INT1_THS;          // Threshold (THS) = 16LSBs * 15.625mg/LSB = 250mg.
+
+    //THRESHOLD
+    writeRegister(0x32, 0x04); //Write 10h into INT1_THS;          // Threshold (THS) = X LSBs * 16 mg/LSB = Y mg.
+    //THRESHOLD
+
+
+    
     writeRegister(0x33, 0x00); //Write 00h into INT1_DURATION;     // Duration = 1LSBs * (1/10Hz) = 0.1s.
     //readRegister();  //Dummy read to force the HP filter to set reference acceleration/tilt value
     writeRegister(0x30, 0x2A); //Write 2Ah into INT1_CFG;          // Enable XLIE, YLIE, ZLIE interrupt generation, OR logic.
@@ -69,6 +75,8 @@ void setup(void) {
   while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
 #endif
 
+  pinMode(15, OUTPUT);
+
   Serial.begin(115200);
   Serial.println("LIS3DH test!");
   
@@ -78,7 +86,7 @@ void setup(void) {
   }
   Serial.println("LIS3DH found!");
   
-  lis.setRange(LIS3DH_RANGE_8_G);   // 2, 4, 8 or 16 G!
+  lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
   
   Serial.print("Range = "); Serial.print(2 << lis.getRange());  
   Serial.println("G");
@@ -88,14 +96,15 @@ void setup(void) {
 void loop() {
   lis.read();      // get X Y and Z data at once
   // Then print out the raw data
-
+  /*
   Serial.print("X:  "); Serial.print(lis.x); 
   Serial.print("  \tY:  "); Serial.print(lis.y); 
   Serial.print("  \tZ:  "); Serial.print(lis.z); 
 
   //Or....get a new sensor event, normalized 
   //Display the results (acceleration is measured in m/s^2) 
-/*
+
+  /*
   sensors_event_t event; 
   lis.getEvent(&event);
    
@@ -112,11 +121,13 @@ void loop() {
     Serial.print(readRegister(0x21)); Serial.print(",  "); //read register to reset high-pass filter 
     Serial.print(readRegister(0x26)); Serial.print(",  "); //read register to set reference acceleration
     Serial.print(readRegister(LIS3DH_REG_INT1SRC)); Serial.print(",  "); //Read INT1_SRC to de-latch;
+    Serial.println();
   }
   else {
-    Serial.print("   \tLOW");
+    //Serial.print("   \tLOW");
+    //Serial.println();
   }
-  Serial.println();
- 
-  delay(200); 
+  digitalWrite(15, HIGH);
+  delay(100); 
+  digitalWrite(15, LOW);
 }
