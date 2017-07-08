1ad6b38f2dc5c9f4ef6f1e4899d014944a878736
Marius Wodtke <marius.wodtke@gmail.com>
2017-07-08 22:06:21 +0200

    Modified server to work with lancom ap

--- server/server.py
+++ server/server.py
@@ -2,18 +2,18 @@
 
 import socket
 import time
-import _thread
+import _thread as thread
 
 def on_new_client (conn, addr):
 		while True:
-			data = conn.recv(32);
+			data = conn.recv(64);
 			if data: print ("received data:", data, "from:", addr);
 			time.sleep(1);
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
 
-s.bind(('192.168.0.150', 8080));
+s.bind(('192.168.200.191', 8080));
 s.listen(5);
 
 try:
