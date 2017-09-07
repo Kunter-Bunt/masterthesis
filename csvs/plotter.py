4ca886043b9323e3fbdde6f556212fbae9db84a9
Marius Wodtke <marius.wodtke@gmail.com>
2017-09-07 19:50:27 +0200

    the new result csvs for lora

--- csvs/plotter.py
+++ csvs/plotter.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z']);
+#data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z']);
+data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y']);
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
@@ -10,6 +11,6 @@ ax1.set_xlabel('Zeit in ms')
 ax1.set_ylabel('Stromverbrauch in mA')
 ax1.plot(data['x'], data['y'], color='r', marker='');
 ax1.fill_between(data['x'], data['y'], 0, facecolor='red', alpha=0.4, interpolate=True)
-ax1.plot(data['x'], data['z'], color='g', marker='');
+#ax1.plot(data['x'], data['z'], color='g', marker='');
 #plt.plotfile('probeReq3ch.csv', delimiter=',', cols=(0, 1), names=('Zeit in ms', 'Stromverbrauch in mA'), marker='.');
 plt.show();
