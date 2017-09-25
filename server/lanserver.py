9135faa79b5ad48a421d1cf79345644a23c1b089
Marius Wodtke <marius.wodtke@gmail.com>
2017-09-25 19:46:12 +0200

    lanserver now implements inteded parsing, no server sindig or quality checks

--- server/lanserver.py
+++ server/lanserver.py
@@ -3,16 +3,31 @@
 import time
 import socket
 import _thread as thread
-from subprocess import call, Popen, PIPE
 import re
 import json
+import pexpect
 
-def readcmd(proc, num):
+verbose = False;
+logging = True;
+
+#the thread function for every ap
+def readcmd(proc):
+	#get mac address of ap
+	proc.send('sysinfo\r\n');
+	apmac = parse_sysinfo(proc);
+	apmac = apmac[0:2] + ":" + apmac[2:4] + ":" + apmac[4:6] + ":" + apmac[6:8] + ":" + apmac[8:10] + ":" + apmac[10:];
+
+	#enable logging
+	proc.send('trace + wlan-data @ ProbeReq\r\n');
+
+	#start parsing the log
 	while True: 
-		dp = parse_line(proc);
-		if dp: s.send(asJSON(dp));
-		time.sleep(0.2);
+		dp = parse_line(proc, apmac);
+		if dp and logging: print(asJSON(dp).decode("UTF-8"));
+		#if dp: s.send(asJSON(dp));
+		time.sleep(0.05);
 
+#___ is separator
 def asJSON(obj):
 	j = json.dumps(obj.__dict__);
 	j += "___";
@@ -25,30 +40,57 @@ class DataPoint:
 		self.stamac = stamac;
 		self.rssi = rssi;
 
+#reads a line from stdout of proc
+def decline(proc): 
+	return proc.readline().decode("UTF-8");
 
-def parse_line(proc):
-	line = proc.stdout.readline().decode("UTF-8");
-	regex = re.compile(r"\w+\.py|build|\d+:\d+:\d+");
-	#regex = re.compile(r"ProbeReq");
-	linelist = re.split("[\s+\n]", line)
-	out2 = list(filter(re.compile(r"\w+\.py|build|\d+:\d+:\d+").search, linelist));
-	if out2: 
-		return DataPoint(1,line,3,4);
+#find mac in sysinfo
+def parse_sysinfo(proc):
+	apmac = "";
+	found = False;
+	while not found:
+		line = decline(proc);
+		if re.match((r"MAC-ADDRESS:."), line): 
+			lines = re.split(r"\s", line);
+			apmac = list(filter(re.compile(r"[a-z0-9]+").search, lines));
+			found = True;
+	if apmac: return apmac[0];
 
+#find wlan-data as package start, read out sender mac and rssi
+def parse_line(proc, apmac):
+	#packet start
+	line = decline(proc);
+	if verbose: print (line);
+	if re.match((r".WLAN-DATA."), line): 
+
+		#only incoming packets
+		line = decline(proc);
+		if verbose: print (line);
+		if re.match((r"Received."), line): 
+			stamac = list(re.findall(r"\w+:\w+:\w+:\w+:\w+:\w+", line));
+			if stamac: 
+				stamac = stamac[0];
+
+				#iterate to "Signal: "
+				for i in range(3):
+					line = decline(proc);
+					if verbose: print (line);
+				rssi = list(re.findall(r"-\d+", line));
+				if rssi: 
+					return DataPoint(time.time(),apmac,stamac,int(rssi[0]));
+				else: print ("Corrupted RSSI");
+			else: print ("Corrupted Station MAC");
+
+#server connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('ideapad', 8080))
+#s.connect(('ideapad', 8080))
 
-processes = [];
+#start the sessions
+for i in range(1):
+	child = pexpect.spawn('ssh -t root@192.168.200.254');
+	child.expect('Password:');
+	child.sendline('key4Lancom!');
+	thread.start_new_thread(readcmd, (child,));
 
-for i in range(5):
-	command = "ls -a";
-	proc = Popen(command.split(), stdout=PIPE);
-	processes.append(thread.start_new_thread(readcmd, (proc, i)));
-#proc = Popen(["tail", "-f", "/var/log/syslog"], stdout=PIPE);
-
-while True:
-	time.sleep(1);
-
-
-
-print ("finished");
+#loop while sessions are open
+while True: time.sleep(1);
