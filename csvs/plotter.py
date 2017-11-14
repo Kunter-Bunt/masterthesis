184ed5f7060f1a5a3d8eb924626ab99efc89dc0a
Marius Wodtke <marius.wodtke@gmail.com>
2017-11-14 16:57:20 +0100

    Small mistake in TDOA and added backup to beamer

--- csvs/plotter.py
+++ csvs/plotter.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-#data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z']);
-#data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z', 'v']);
 #data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y']);
-data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z', 'v', 'w', 'u']);
+#data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z']);
+data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z', 'v']);
+#data = np.genfromtxt(sys.argv[1], delimiter=',', names=['x', 'y', 'z', 'v', 'w', 'u']);
+
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
@@ -18,8 +19,8 @@ ax1.fill_between(data['x'], data['y'], 0, facecolor='r', alpha=0.4, interpolate=
 ax1.plot(data['x'], data['y'], color='r', marker='');
 ax1.plot(data['x'], data['z'], color='g', marker='');
 ax1.plot(data['x'], data['v'], color='b', marker='');
-ax1.plot(data['x'], data['w'], color='k', marker='');
-ax1.plot(data['x'], data['u'], color='m', marker='');
+#ax1.plot(data['x'], data['w'], color='k', marker='');
+#ax1.plot(data['x'], data['u'], color='m', marker='');
 
 #plt.plotfile('probeReq3ch.csv', delimiter=',', cols=(0, 1), names=('Zeit in ms', 'Stromverbrauch in mA'), marker='.');
 plt.show();
